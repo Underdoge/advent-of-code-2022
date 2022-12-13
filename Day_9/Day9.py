e960a734a01e66f769b1db71811253d956a838cf
Underdoge <echapa@live.com.mx>
2022-12-13 00:00:22 -0600

    Day 9 Part 1

--- Day_9/Day9.py
+++ Day_9/Day9.py
@@ -30,7 +30,7 @@ width = screen.get_width()
 height = screen.get_height()
 
 # game title
-milisecs = 500
+milisecs = 1
 speed = 60
 
 count = 0
@@ -38,6 +38,7 @@ rgb = random.choice(color_list)
 
 # function for body of the game
 def rope(dir,distance):
+    tail_positions = []
     instruction = 1
     block_size = 10
     df_movements = pd.DataFrame({'dir':dir,'steps':distance})
@@ -57,25 +58,40 @@ def rope(dir,distance):
     
     time = int(pygame.time.get_ticks() / milisecs) + 2 # wait 1 second before starting
     smallfont = pygame.font.SysFont('Anonymice Powerline', 35)
-    text2 = smallfont.render(f"Press 'q' to exit or 'r' to restart...", True, white)
-    text2_x,text2_y = smallfont.size(f"Press 'q' to exit or 'r' to restart...")
+    text2 = smallfont.render(f"Press 'r' to restart, 'p' to pause or 'q' to exit ...", True, white)
+    text2_x,text2_y = smallfont.size(f"Press 'r' to restart, 'p' to pause or 'q' to exit ...")
+    text3 = smallfont.render(f"Press space to start...", True, white)
+    text3_x,text3_y = smallfont.size(f"Press space to start...")
     screen.fill((65, 25, 64))
+    screen.blit(text3, (res[0]/2 - (text3_x/2), res[1]-100))
     
-    running = True
     key = pygame.key.get_pressed()
+    running = True
+    started = False
+    
+    
     while running and not key[pygame.K_q]:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE]:
+            started = True
+        if key[pygame.K_p]:
+            if started: # pause
+                started = False
+            else:
+                started = True
 
         pygame.draw.rect(screen, start_c, [start_x, start_y, block_size,block_size])         
         pygame.draw.rect(screen, head_c, [head_x, head_y, block_size,block_size])   
 
-        if int(pygame.time.get_ticks() / milisecs) > time:
-            time = int(pygame.time.get_ticks() / milisecs)
+        if started:
+            #time = int(pygame.time.get_ticks() / milisecs)
             if key[pygame.K_r]: # restart
+                tail_positions = []
+                instruction = 1
                 head_x = int(res[0]/2) - int((max_steps * block_size ) / 2)
                 head_y = int(res[1]/2) + int((max_steps * block_size ) / 2)
                 start_x = head_x
@@ -85,16 +101,21 @@ def rope(dir,distance):
                 df_movements = pd.DataFrame({'dir':dir,'steps':distance})
                 steps = df_movements.iloc[0]['steps']
                 dist = steps
-                time += 1
+                #time += 1
+            if key[pygame.K_p]: # pause
+                started = False
+                
             if len(df_movements) == 0 and steps == dist:
                 if key[pygame.K_q]: # quit
                     running = False
-
                 screen.fill((65, 25, 64))
                 pygame.draw.rect(screen, start_c, [start_x, start_y, block_size,block_size])         
                 pygame.draw.rect(screen, head_c, [head_x, head_y, block_size,block_size])
                 screen.blit(text, (res[0]/2 - (text_x/2), res[1]-100))
                 screen.blit(text2, (res[0]/2 - (text2_x/2), res[1]-50))
+                text4 = smallfont.render(f"Tail positions: {len(tail_positions)}", True, white)
+                text4_x,text4_y = smallfont.size(f"Tail positions: {len(tail_positions)}")
+                screen.blit(text4, (res[0]/2 - (text4_x/2), res[1]-150))
             else:
                 screen.fill((65, 25, 64))
                 if steps == dist:
@@ -118,21 +139,38 @@ def rope(dir,distance):
                         head_x += direction * block_size
                     else:
                         head_y += direction * block_size
+
                 pygame.draw.rect(screen, start_c, [start_x, start_y, block_size,block_size])
                 pygame.draw.rect(screen, head_c, [head_x, head_y, block_size,block_size])
+                if ((tail_x,tail_y) not in tail_positions):
+                    tail_positions.append((tail_x,tail_y))
                 if (tail_x < head_x - block_size):
+                    #print ("case a")
                     tail_x = head_x - block_size
-                if (tail_x > head_x + 2 * block_size):
-                    tail_x = head_x + block_size
-                if (tail_y != head_y):
+                    if tail_y != head_y:
+                     #   print ("case b")
+                        tail_y = head_y
+                if (tail_x > head_x + block_size):
+                    #print ("case d")
                     tail_y = head_y
+                    tail_x = head_x + block_size
+                if (tail_y < head_y - block_size):
+                    #print(f"Tail_x {tail_x} Tail_y {tail_y} Head_x {head_x} Head_y {head_y}")
+                    #print ("case e")
+                    tail_x = head_x
+                    tail_y = head_y - block_size
+                   # print(f"Tail_x {tail_x} Tail_y {tail_y} Head_x {head_x} Head_y {head_y}")
+                if (tail_y > head_y + block_size):
+                    #print ("case f")
+                    tail_x = head_x
+                    tail_y = head_y + block_size
+
                 pygame.draw.rect(screen, tail_c, [tail_x, tail_y, block_size,block_size])
                     
                 text_x,text_y = smallfont.size(f"{instruction}: {movement['dir']} {movement['steps']}")
                 screen.blit(text, (res[0]/2 - (text_x/2), res[1]-100))
-                text2_x,text2_y = smallfont.size(f"Press 'q' to exit or 'r' to restart...")
+                text2_x,text2_y = smallfont.size(f"Press 'r' to restart, 'p' to pause or 'q' to exit ...")
                 screen.blit(text2, (res[0]/2 - (text2_x/2), res[1]-50))
-
         clock.tick(speed)
         pygame.display.update()
         
