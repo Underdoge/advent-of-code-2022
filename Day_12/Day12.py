84e9c56bba91b8945faf073a2874ae29f65bee71
Eduardo Chapa <eduardo.chapa@gmail.com>
2023-01-05 20:29:05 -0600

    Day 12

--- Day_12/Day12.py
+++ Day_12/Day12.py
@@ -3,6 +3,25 @@ def print_map(map):
     for line in map:
         print(line)
 
+def print_steps(map,steps):
+    print("\nLen(steps):",len(steps))
+    for line in map:
+        for char in line:
+            if (map.index(line),line.index(char)) in steps:
+                print(steps[(map.index(line),line.index(char))],end="")
+            else:
+                print(".",end="")
+        print("")
+    print("")
+    for line in map:
+        for char in line:
+            if (map.index(line),line.index(char)) in steps:
+                print(char,end="")
+            else:
+                print(".",end="")
+        print("")
+
+
 def read_map(file):
     map = []
     for line in open(file):
@@ -25,35 +44,33 @@ def find_end(map):
             if char == 'E':
                 return line.index(char),map.index(line),
 
-def walk_map(start,end,steps,map):
-    steps.append(start)
-    if start != end:
-        print("Start:",start,"End:",end,"Steps:",steps,"len(map[0])",len(map[0]),"len(map)",len(map))
-        start_x,start_y = start
-        end_x,end_y = end
-        if start_x + 1 < len(map[0]) and (ord(map[start_y][start_x]) >= ord(map[start_y][start_x + 1]) or ord(map[start_y][start_x]) == ord(map[start_y][start_x + 1]) + 1):
-            print("Right")
-            walk_map((start_y,start_x + 1),end,steps,map)
-        if start_x - 1 > 0 and (ord(map[start_y][start_x]) >= ord(map[start_y][start_x - 1]) or ord(map[start_y][start_x]) == ord(map[start_y][start_x - 1]) + 1):
-            print("Left")
-            walk_map((start_y,start_x - 1),end,steps,map)
-        if start_y + 1 < len(map) and (ord(map[start_y][start_x]) >= ord(map[start_y + 1][start_x]) or ord(map[start_y][start_x]) == ord(map[start_y + 1][start_x]) + 1):
-            print("Down")
-            walk_map((start_y,start_x - 1),end,steps,map)
-        if start_y - 1 > 0 and (ord(map[start_y][start_x]) >= ord(map[start_y - 1][start_x ]) or ord(map[start_y][start_x]) == ord(map[start_y - 1][start_x]) + 1):
-            print("Up")
-            walk_map((start_y,start_x - 1),end,steps,map)
+def walk_map(start,steps,map):
+    results = []
+    start_y,start_x = start
+    if map[start_y][start_x] != 'E':
+        if (start_y,start_x + 1) not in steps and start_x + 1 <= len(map[0]) - 1 and map[start_y][start_x + 1] != 'S' and (map[start_y][start_x] == 'S' or ord(map[start_y][start_x]) >= ord(map[start_y][start_x + 1]) or ord(map[start_y][start_x]) + 1 == ord(map[start_y][start_x + 1])):
+            #print("Right","start_y",start_y,"start_x",start_x,"letter",map[start_y][start_x],"comparing to",map[start_y][start_x + 1],"ord(map[start_y][start_x])",ord(map[start_y][start_x]),"ord(map[start_y][start_x + 1])",ord(map[start_y][start_x + 1]))
+            steps[start] = ">"
+            walk_map((start_y,start_x + 1),steps.copy(),map)
+        if (start_y,start_x - 1) not in steps and start_x - 1 >= 0 and map[start_y][start_x - 1] != 'S' and (map[start_y][start_x] == 'S' or ord(map[start_y][start_x]) >= ord(map[start_y][start_x - 1]) or ord(map[start_y][start_x]) + 1 == ord(map[start_y][start_x - 1])):
+            #print("Left","start_y",start_y,"start_x",start_x,"letter",map[start_y][start_x],"comparing to",map[start_y][start_x - 1],"ord(map[start_y][start_x])",ord(map[start_y][start_x]),"ord(map[start_y][start_x - 1])",ord(map[start_y][start_x - 1]))
+            steps[start] = "<"
+            walk_map((start_y,start_x - 1),steps.copy(),map)
+        if (start_y + 1,start_x) not in steps and start_y + 1 <= len(map) - 1 and map[start_y + 1][start_x] != 'S' and (map[start_y][start_x] == 'S' or ord(map[start_y][start_x]) >= ord(map[start_y + 1][start_x]) or ord(map[start_y][start_x]) + 1 == ord(map[start_y + 1][start_x])):
+            #print("Down","start_y",start_y,"start_x",start_x,"letter",map[start_y][start_x],"comparing to",map[start_y + 1][start_x],"ord(map[start_y][start_x])",ord(map[start_y][start_x]),"ord(map[start_y - 1][start_x])",ord(map[start_y - 1][start_x]))
+            steps[start] = "v"
+            walk_map((start_y + 1,start_x),steps.copy(),map)
+        if (start_y - 1,start_x) not in steps and start_y - 1 >= 0 and map[start_y - 1][start_x] != 'S' and (map[start_y][start_x] == 'S' or ord(map[start_y][start_x]) >= ord(map[start_y - 1][start_x]) or ord(map[start_y][start_x]) + 1 == ord(map[start_y - 1][start_x])):
+            #print("Up","start_y",start_y,"start_x",start_x,"letter",map[start_y][start_x],"comparing to",map[start_y - 1][start_x],"ord(map[start_y][start_x])",ord(map[start_y][start_x]),"ord(map[start_y - 1][start_x])",ord(map[start_y - 1][start_x]))
+            steps[start] = "^"
+            walk_map((start_y - 1,start_x),steps.copy(),map)
     else:
-        return steps
+        steps[start] = "E"
+        print_steps(map,steps)
         
+print_map(read_map("/Users/echapa/advent-of-code-2022/Day_12/test.txt"))
+print(find_start(read_map("/Users/echapa/advent-of-code-2022/Day_12/test.txt")))
+print(find_end(read_map("/Users/echapa/advent-of-code-2022/Day_12/test.txt")))
+start = find_start(read_map("/Users/echapa/advent-of-code-2022/Day_12/test.txt"))
+walk_map(start,{},read_map("/Users/echapa/advent-of-code-2022/Day_12/test.txt"))
 
-    
-print_map(read_map("test.txt"))
-
-print(find_start(read_map("test.txt")))
-print(find_end(read_map("test.txt")))
-
-start = find_start(read_map("test.txt"))
-end = find_end(read_map("test.txt"))
-
-print("Steps:",walk_map(start,end,[],read_map("test.txt")))
